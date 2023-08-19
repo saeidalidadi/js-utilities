f602e932cf917223eeb516811b448743767699dd
heartbit <alidadisaeid@gmail.com>
2023-08-19 12:18:24 +0330

    feat: set formatter configuration

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,10 @@ import _isObject from "lodash.isobject";
 
 export const combineURLs = (baseURL: string, relativeURL: string): string => {
     const url = relativeURL
-        ? baseURL.replace(/\/+$/, "") + '/' + relativeURL.replace(/^\/+/, '')
+        ? baseURL.replace(/\/+$/, "") + "/" + relativeURL.replace(/^\/+/, "")
         : baseURL;
     return url;
-}
+};
 
 /**
  * Replace other chars with `_`
@@ -22,10 +22,10 @@ export const combineURLs = (baseURL: string, relativeURL: string): string => {
  * @returns {string} with `_`
  */
 export const withUnderscore = (str: string): string => {
-    let pattern = /[^a-z0-9]+|\s+/gmi;
+    let pattern = /[^a-z0-9]+|\s+/gim;
     let result = str.replace(pattern, "_");
     return result;
-}
+};
 
 /**
  * get response of fetch always as json object.
@@ -33,16 +33,16 @@ export const withUnderscore = (str: string): string => {
  */
 export const fetchJsonRes = async (response: any) => {
     try {
-        let text = await response.text()
+        let text = await response.text();
         return await JSON.parse(text);
     } catch (err: any) {
-        throw new Error("The response data is not as JSON format(" + err.message + ")")
+        throw new Error("The response data is not as JSON format(" + err.message + ")");
     }
-}
+};
 
 export const resolvedOptions = () => {
     return Intl.DateTimeFormat().resolvedOptions();
-}
+};
 
 /**
  * Find user's browser locale code
@@ -51,7 +51,7 @@ export const resolvedOptions = () => {
 export const getLocale = (): string => {
     const { locale } = resolvedOptions();
     return locale;
-}
+};
 
 /**
  * Find users's browser time zone
@@ -60,17 +60,16 @@ export const getLocale = (): string => {
 export const getTimeZone = (): string => {
     const { timeZone } = resolvedOptions();
     return timeZone;
-}
+};
 
 /**
- * Set source of image tag 
+ * Set source of image tag
  * @param {string|Object} src - the remote url or local image file
  * @return {string}
  */
 export const toImageSource = (src: string | File): string => {
-    return typeof src === 'string' ? src : URL.createObjectURL(src);
-}
-
+    return typeof src === "string" ? src : URL.createObjectURL(src);
+};
 
 /**
  * Deep diff between two objects, using lodash
@@ -83,20 +82,21 @@ export const objectsDiff = (object: any, base: any) => {
     function changes(object: any, base: any) {
         return _transform(object, function (result: any, value, key) {
             if (!_isEqual(value, base[key])) {
-                result[key] = (_isObject(value) && _isObject(base[key])) ? changes(value, base[key]) : value;
+                result[key] =
+                    _isObject(value) && _isObject(base[key]) ? changes(value, base[key]) : value;
             }
         });
     }
     return changes(object, base);
-}
+};
 
 /**
  * Delay event loop
  * @param {number} ms - waiting duration as milliseconds
  */
 export const delay = (ms: number) => {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-}
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 /**
  * Creates delay with random durations
@@ -104,13 +104,13 @@ export const delay = (ms: number) => {
  */
 export const randomDelay = async (range: number[] | number = 1000) => {
     let randomMS;
-    if (typeof range === 'number') {
+    if (typeof range === "number") {
         randomMS = Math.round(Math.random() * range);
     } else {
         randomMS = Math.floor(Math.random() * (range[1] - range[0]) + range[0]);
     }
     return await delay(randomMS);
-}
+};
 
 /**
  * Create ranges between tow numbers or characters
@@ -118,11 +118,11 @@ export const randomDelay = async (range: number[] | number = 1000) => {
  * @param end
  */
 export const range = (start: number, end: number): Array<number> => {
-    if(start > end) {
-        throw new Error("starting number should be less than end number")
+    if (start > end) {
+        throw new Error("starting number should be less than end number");
     }
-    return Array.from({length: end  - start}, (value, index) => start + index)
-}
+    return Array.from({ length: end - start }, (value, index) => start + index);
+};
 
 /**
  * Create ranges between tow numbers or characters
@@ -130,15 +130,16 @@ export const range = (start: number, end: number): Array<number> => {
  * @param end the ending point letter(inclusive)
  */
 export const rangeLetters = (start: string, end: string) => {
+    // prettier-ignore
     const letters = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
         'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     ];
-    
+
     let sli = letters.indexOf(start);
     let eli = letters.indexOf(end);
-    let res =  letters.filter((letter, index) => index <= sli || eli >= index);
+    let res = letters.filter((letter, index) => index <= sli || eli >= index);
 
-    return res;    
-}
+    return res;
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,30 @@
-import { combineURLs, delay, fetchJsonRes, objectsDiff, range, rangeLetters, withUnderscore } from "../src";
+import {
+    combineURLs,
+    delay,
+    fetchJsonRes,
+    objectsDiff,
+    range,
+    rangeLetters,
+    withUnderscore,
+} from "../src";
 import fetchMock from "jest-fetch-mock";
 
-
 describe("Strings", () => {
-    describe('combineURLs', () => {
-        test('should return valid URL if the path starts with double slashes', () => {
-            const result = combineURLs("http://test.pass", "//this/test")
+    describe("combineURLs", () => {
+        test("should return valid URL if the path starts with double slashes", () => {
+            const result = combineURLs("http://test.pass", "//this/test");
 
             expect(result).toEqual("http://test.pass/this/test");
         });
 
-        test('should return valid URL if there are not any slashes', () => {
-            const result = combineURLs("http://test.pass", "this/test")
+        test("should return valid URL if there are not any slashes", () => {
+            const result = combineURLs("http://test.pass", "this/test");
 
             expect(result).toEqual("http://test.pass/this/test");
         });
 
-        test('should return valid URL if there are slashes at the end and at the start of url parts', () => {
-            const result = combineURLs("http://test.pass/", "/this/test")
+        test("should return valid URL if there are slashes at the end and at the start of url parts", () => {
+            const result = combineURLs("http://test.pass/", "/this/test");
 
             expect(result).toEqual("http://test.pass/this/test");
         });
@@ -26,72 +33,70 @@ describe("Strings", () => {
         test("Should split string with `_`.", () => {
             let result = withUnderscore("onstage-js-utils");
 
-            expect(result.split('_')).toHaveLength(3);
+            expect(result.split("_")).toHaveLength(3);
 
             result = withUnderscore("onstage.js.utils");
 
-            expect(result.split('_')).toHaveLength(3);
-        })
-    })
-})
-
-describe('Objects & Arrays', () => {
-    test('Should return changed props of the base object', () => {
-        const base = {
-            price: 99.9,
-            type: "product"
-        }
-
-        const changed = {
-            price: 99.9,
-            type: "orders"
-        }
-
-        const result = objectsDiff(changed, base)
-
-        expect(result).toMatchObject({ type: "orders" })
+            expect(result.split("_")).toHaveLength(3);
+        });
     });
 });
 
+describe("Objects & Arrays", () => {
+    test("Should return changed props of the base object", () => {
+        const base = {
+            price: 99.9,
+            type: "product",
+        };
 
-describe('Parsers', () => {
-    test('Should return JSON if the response is in JSON format.', async () => {
-        const data = { naem: "John" }
-        fetchMock.mockResponseOnce(JSON.stringify(data))
+        const changed = {
+            price: 99.9,
+            type: "orders",
+        };
 
-        const result = await fetch("/api")
-        const jsonObject = await fetchJsonRes(result)
-        expect(jsonObject).toMatchObject(data)
+        const result = objectsDiff(changed, base);
+
+        expect(result).toMatchObject({ type: "orders" });
+    });
+});
+
+describe("Parsers", () => {
+    test("Should return JSON if the response is in JSON format.", async () => {
+        const data = { naem: "John" };
+        fetchMock.mockResponseOnce(JSON.stringify(data));
+
+        const result = await fetch("/api");
+        const jsonObject = await fetchJsonRes(result);
+        expect(jsonObject).toMatchObject(data);
     });
 
-    test('Should throw Error if the response is a text.', async () => {
-        fetchMock.mockResponseOnce("ok")
+    test("Should throw Error if the response is a text.", async () => {
+        fetchMock.mockResponseOnce("ok");
 
-        const res = await fetch("/api")
-        await expect(async () => await fetchJsonRes(res)).rejects.toThrow(Error)
-
+        const res = await fetch("/api");
+        await expect(async () => await fetchJsonRes(res)).rejects.toThrow(Error);
     });
 });
 
 describe("Timers & Delay", () => {
     describe("Delay", () => {
-        test('should delay procedure for 1 second', async () => {
-            jest.spyOn(global, 'setTimeout')
-            await delay(1000)
+        test("should delay procedure for 1 second", async () => {
+            jest.spyOn(global, "setTimeout");
+            await delay(1000);
             expect(setTimeout).toHaveBeenCalledTimes(1);
-            expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
+            expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
         });
-    })
-})
+    });
+});
 
 describe("Ranges", () => {
     it("Should return an array of numbers between two numbers including start point", () => {
         const numbers = range(1, 5);
-        expect(numbers).toEqual([1, 2, 3, 4])
-    })
+        expect(numbers).toEqual([1, 2, 3, 4]);
+    });
 
     it("Should return characters between tow letters", () => {
-        const letters = rangeLetters('a', 'd');
-        expect(letters).toEqual(['a', 'b', 'c', 'd']);
-    })
-})
+        const letters = rangeLetters("a", "d");
+        expect(letters).toEqual(["a", "b", "c", "d"]);
+    });
+});
